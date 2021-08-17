8b91703c4d1c43c4d8fbfb5e04c752b9d780cd88
yangdongjue5510 <djwhy5510@gmail.com>
2021-08-17 11:03:41 +0900

    fix: .gitignore fix

---
+++ DesignPattern/src/Decorator/HouseBlend.java
@@ -0,0 +1,10 @@
+package Decorator;
+
+public class HouseBlend extends Beverage{
+    public HouseBlend(){
+        description = "House Blend COffee";
+    }
+    public double cost(){
+        return .89;
+    }
+}
