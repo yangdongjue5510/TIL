013108f246778cd552e2b5673c5c234bb180994c
yangdongjue5510 <djwhy5510@gmail.com>
2021-09-27 16:02:02 +0900

    210927

--- Algorithm/Codes/src/week4/No2138.java
+++ Algorithm/Codes/src/week4/No2138.java
@@ -5,45 +5,41 @@ import java.io.*;
 public class No2138{
     static char[] arr1;
     static char[] arr2;
-    static char[] arr3;
-    public static int change(char[] a){
-        int result = 0;
-        for(int i = 1 ; i < a.length ; i++){
-            if(arr2[i] != a[i]){
-                if(i==a.length-1){
-                    a[i-1]= (a[i-1]=='0')?'1':'0';
-                    a[i]= (a[i]=='0')?'1':'0';
-                }
-                else{
-                    a[i-1]= (a[i-1]=='0')?'1':'0';
-                    a[i]= (a[i]=='0')?'1':'0';
-                    a[i+1]= (a[i+1]=='0')?'1':'0';
-                }
-                result++;
+    static char[] target;
+    public static void change(int index, char[] arr){
+        arr[index-1] =(arr[index-1]=='0') ? '1':'0';
+        arr[index] =(arr[index]=='0') ? '1':'0';
+        if(index != arr.length-1){
+            arr[index+1] =(arr[index+1]=='1') ? '0':'1';
+        }
+    }
+    public static int test(char[] arr){
+        int count = 0;
+        for(int i = 0; i < arr.length-1 ; i++){
+            if(arr[i]!=target[i]){
+                change(i+1, arr);
+                count ++;
             }
         }
-        if(Arrays.equals(arr2, a))
-            result = -1;
+        int result = (Arrays.equals(arr, target)) ? count : -1;
         return result;
     }
-    public static void main(String[] args) throws IOException{
-        BufferedReader bf = new BufferedReader(
-                new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bf.readLine());
         arr1 = bf.readLine().toCharArray();
-        arr2 = bf.readLine().toCharArray();
-        arr3 = arr1.clone();
-        arr3[0] =  (arr3[0] == '0') ? '1':'0';
-        arr3[1] =  (arr3[1] == '0') ? '1':'0';
-        int result1 = change(arr1);
-        int result2 = change(arr3);
-        if (result1 != -1 && result2 != -1)
-            System.out.println(Math.min(result1, result2));
-        else if(result1 != -1)
-            System.out.println(result1);
-        else if(result2 != -1)
-            System.out.println(result2);
-        else
-            System.out.println(-1);
+        arr2 = arr1.clone();
+        arr2[0] = (arr2[0]=='0') ? '1':'0';
+        arr2[1] = (arr2[1]=='0') ? '1':'0';
+        target = bf.readLine().toCharArray();
+        int count1 = test(arr1);
+        int count2 = test(arr2);
+        int result = -1;
+        if(count1 != -1 && count2 != -1){
+            result = Math.min(count1, count2+1);
+        }
+        else if(count1 != -1) result = count1;
+        else if(count2 != -1) result = count2+1;
+        System.out.println(result);
     }
 }
