d055ba3f99a2e6948f6dbd320fa2d17f2fbc6190
yangdongjue5510 <djwhy5510@gmail.com>
2021-10-06 17:11:13 +0900

    model appended v3

---
+++ Spring_Boot/Inflearn/MVC1/servlet/src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberFormControllerV3.java
@@ -0,0 +1,14 @@
+package hello.servlet.web.frontcontroller.v3.controller;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+
+import java.util.Map;
+
+public class MemberFormControllerV3 implements ControllerV3 {
+
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        return new ModelView("new-form"); //뷰의 경로가 아닌 논리적인 이름만 넣어준다.
+    }
+}
